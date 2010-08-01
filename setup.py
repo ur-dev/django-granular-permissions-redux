cc053a5c4ff84d3c8229fd6685302e2ce4566a22
Andrew Rowls <andrew@eternicode.com>
2010-08-01 00:53:06 -0400

    Rearranged package and added setup.py to make package pip-installable.
    --HG--
    rename : __init__.py => django-granular-permissions/__init__.py
    rename : models.py => django-granular-permissions/models.py
    rename : templatetags/__init__.py => django-granular-permissions/templatetags/__init__.py
    rename : templatetags/row_level_perms.py => django-granular-permissions/templatetags/row_level_perms.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django granular permissions',
+      version='0.4',
+      description='Row-level permissions for Django',
+      author='Yazzgoth@googlecode, Ryates@github, Eternicode@bitbucket',
+      url='http://bitbucket.org/eternicode/django-granular-permissions',
+      packages=['django-granular-permissions'],
+     )
+
