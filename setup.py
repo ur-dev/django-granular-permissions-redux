4656566fb35cb5234eda7f42b8b20ad87e355128
Ian A Wilson <ian.a.wilson@rochester.edu>
2012-08-09 11:58:49 -0400

    Updated setup.py to include some info about us and to fix a bug where templatetags were not installed. This closes dev.lasso.rochester.edu/issues/218

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='django granular permissions',
       version='0.4',
       description='Row-level permissions for Django',
-      author='Yazzgoth@googlecode, Ryates@github, Eternicode@bitbucket',
-      url='http://bitbucket.org/eternicode/django-granular-permissions',
-      packages=['granular_permissions'],
+      author='Yazzgoth@googlecode, Ryates@github, Eternicode@bitbucket, ianawilson@github',
+      url='https://github.com/ur-dev/django-granular-permissions-redux',
+      packages=['granular_permissions', 'granular_permissions.templatetags'],
      )
 
